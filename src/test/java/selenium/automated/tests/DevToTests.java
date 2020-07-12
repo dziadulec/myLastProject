052b784322206105221fec333630b0aa8d67708c
dziadulec <dzadzumaster@gmail.com>
2020-07-12 12:16:12 +0200

    lastchange

--- src/test/java/selenium/automated/tests/DevToTests.java
+++ src/test/java/selenium/automated/tests/DevToTests.java
@@ -3,81 +3,65 @@ package selenium.automated.tests;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class DevToTests {
 
     WebDriver driver;
+    String url = "https://dev.to/";
+
+    public void HighlightElement(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].setAttribute('style', 'border: 2px solid red;');", element);
+    }
+
 
     @Before
-    public void SetUp(){
+    public void SetUp() {
 
         System.setProperty("webdriver.chrome.driver", "c://SeleniumWebdriver//chromedriver.exe");
         driver = new ChromeDriver();
-
+        driver.get(url); //otworzenie linku w przeglądarce
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // jeśli nie mozesz znalezc elememtu, poczekaj 10 sek, sprawdzajac co 0.5 sek, czy element jest juz dostepny
     }
     @Test
-    public void FirstTest(){
-
-        String url = "https://dev.to/";
-        driver.get(url);
-
-
-        String curentUrl = driver.getCurrentUrl();
-
-        assertTrue("The current URL isn't dev.to!",url.equals(curentUrl));
-
-
+    public void OpenDevTo() {
+        //na ten moment, mamy otworzną stronę dev.to - żeby sprawdzić, czy faktycznie na niej jesteśmy,
+        // chcemy porównać url ze zmiennej wcześniejszej do obecnego url z przeglądarki
+        String currentUrl = driver.getCurrentUrl(); //wyciągamy obecny url z przeglądarki i przypisujemy go do zmiennej currentUrl
+        //assertTrue - sprawdza poprawność warunku url.equals(currentUrl) - czy url ze zmiennej url jest taki sam jak w zmiennej currentUrl
+        //jeśli nie - wypisuje message "The current url isn't dev.to", i ustawia test na fail
+        assertTrue("The current url isn't dev.to", url.equals(currentUrl));
     }
-
     @Test
-    public void weekclick(){
-
-        String url = "https://dev.to/top/week";
-        driver.get(url);
-
-
-        String curentUrl = driver.getCurrentUrl();
-
-        //assertTrue("The current URL isn't dev.to!",url.equals(curentUrl));
-
-        driver.findElement(By.xpath("//*[@id=\"on-page-nav-controls\"]/div/nav/a[2]")).click();
-        String curentUrl2 = driver.getCurrentUrl();
-
-        assertTrue("The current URL isn't dev.to!",url.equals(curentUrl2));
-
-
+    public void GoToWeek() {
+        WebElement week = driver.findElement(By.cssSelector("#on-page-nav-controls > div > nav > a:nth-child(2)")); //znalezienie elementu week na stronie
+        HighlightElement(week); // podswietlenie elementu week
+        week.click(); // klikniecie elementu week
+        WebDriverWait wait = new WebDriverWait(driver, 5); //zainicjalizowanie Explicit Wait
+        wait.until(ExpectedConditions.urlToBe("https://dev.to/top/week")); //poczekaj aż url będzie : https://dev.to/top/week
+        //wait.until(ExpectedConditions.attributeContains(week,"class","item--current"));
+        WebElement firstPostOnWeek = driver.findElement(By.className("crayons-story__body")); //odnalezienie pierwszego posta
+        HighlightElement(firstPostOnWeek); //podswietlenie 1 postu
+        WebElement firstPostTitle = driver.findElement(By.cssSelector(".crayons-story__title > a")); //znajdź element za pomocą cssSelector -  będzie to nazwa 1 posta
+        HighlightElement(firstPostTitle);
+        String linkToFirstPost = firstPostTitle.getAttribute("href"); //wyciagnij z nazwy pierwszego posta link do strony
+        firstPostOnWeek.click(); //klikniecie 1 postu
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("crayons-article__header__meta"))); // poczekaj, aż
+        String currentUrl = driver.getCurrentUrl(); //wyciagnij obecny link
+        //sprawdz czy link do postu jest taki sam jak obecny url
+        assertEquals("url isn't the same as link(href) value", linkToFirstPost, currentUrl);
     }
-
-    @Test
-    public void searchclick(){
-
-        String url = "https://dev.to/";
-        driver.get(url);
-
-
-
-        //assertTrue("The current URL isn't dev.to!",url.equals(curentUrl));
-
-        driver.findElement(By.id("nav-search")).sendKeys("java");
-        driver.findElement(By.id("nav-search")).sendKeys(Keys.ENTER);
-
-        String curentUrl2 = driver.getCurrentUrl();
-
-        String curentUrl = driver.getCurrentUrl();
-
-        //assertTrue("The current URL isn't dev.to!",url.equals(curentUrl2));
-
-        //driver.close();
-
-
-    }
-
 
 
 }
